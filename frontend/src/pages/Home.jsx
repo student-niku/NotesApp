28cb107de7a3f3ca4a58d0d232d5ff9fc981525f
student-niku <kniku4567@gmail.com>
2026-07-25 18:13:11 +0530

    first commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import NavBar from '../components/Navbar/navbar'
-import HeroSection from '../components/HeroComponent/heroSection'
+import NavBar from '../components/Navbar/Navbar'
+import HeroSection from '../components/HeroComponent/HeroSection'
 import { useLocation } from "react-router-dom";
 const Home = () => {
    const location = useLocation();
